5e78c85f9a7de510b76f7b4f123ea36de9ae6cc9
Puru Dahal <pdahal@me.com>
2014-07-17 17:08:05 -0700

    User signup/login routes/controller/views

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,24 @@
+#POST Signup
+get '/signup' do
+	erb:'/users/signup'
+end
+
+post '/users/signup' do
+	User.create(params)
+	redirect '/login'
+end
+
+#GET Signin
+get '/login' do
+	erb:'/users/login'
+end
+
+post '/users/login' do
+	@user = User.find_by_email(params[:email])
+	if @user.password == params[:password]
+		'Logged In'
+	else
+		#redirect '/signup'
+		'Something went wrong...'
+	end
+end
